3e21c55106dbebcd8f35782b4539a30bbf405b6c
Ashwincoder3 <74143551+Ashwincoder3@users.noreply.github.com>
2021-10-19 15:03:02 +0530

    Update UserService.java

--- Employee/src/com/training/bean/UserService.java
+++ Employee/src/com/training/bean/UserService.java
@@ -3,34 +3,53 @@ package com.training.bean;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class UserService {
-	public static void main(String[] args) {
-		User us = new User(111,"ashwin","ashwin@gmail.com");
-		User us1=new User(222,"bharadwaj","bharadwaj@gmail.com");
-		User us2=new User(333,"vishal","vishal@gmail.com");
-		String id;
-		Scanner sc=new Scanner(System.in);
-		List<User> lst=new ArrayList<User>();
-		lst.add(us);
-		lst.add(us1);
-		lst.add(us2);
-		System.out.print("\n total number of employees :"+lst.size());
-		
-		System.out.print("\n  employees details  :\n"+lst);
-		System.out.print("\n is no records is there ? : "+lst.isEmpty());
-		//System.out.print("\nenter obj to search :");
-		//id=sc.next();
-		System.out.println("\n user one is available ?"+lst.contains(us));
-		lst.remove(0);
-		System.out.print("\n  employees details  :\n"+lst);
-		Iterator it=lst.iterator();
-		while(it.hasNext()) {
-			System.out.print("\n"+it.next());
-		}
-		
+	static Scanner sc=new Scanner(System.in);
+	List<User> lst=new ArrayList<User>();
+	public  void add() {
+		User ashwin = new User(111,"ashwin","ashwin@gmail.com");
+		User bharadwaj=new User(222,"bharadwaj","bharadwaj@gmail.com");
+		User vishal=new User(333,"vishal","vishal@gmail.com");
+		lst.add(ashwin);
+		lst.add(bharadwaj);
+		lst.add(vishal);
 	}
+
+
+	public void delete() {
+		String choice="Y",obj;
+		while(choice.equalsIgnoreCase("y")){
+		System.out.println("enter employee object to delete: ");
+		obj=sc.next();
+		User object1 = null;
+		object1.setUserName(obj);
+		lst.remove(object1);
+		
+		System.out.println("you want to continue :y or no :n");
+		choice=sc.next();
+		}
+		System.out.println("employee is removed :");
+	}	
+
+public void displayAllEmployees(){
+	ListIterator li=lst.listIterator();
+	System.out.println("employee in details");
+	while(li.hasNext()){
+		System.out.printf("\n"+li.next());
+	}	
+  }
+public void searchEmployee(){
+	String ob;
+	System.out.println("enter employee object to search: ");
+	ob=sc.next();
+	User object = null;
+	object.setUserName(ob);
+	lst.indexOf(object);
+	System.out.println("employee in the index of :"+lst.indexOf(object));
+}
+}	
 	
 
-}
